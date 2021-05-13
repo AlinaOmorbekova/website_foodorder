80d90090710776a10d88858d39df7e495b11aa5a
Alina <46772913+AlinaOmorbekova@users.noreply.github.com>
2021-05-13 19:50:41 +0600

    Add files via upload

---
+++ manage-food.php
@@ -0,0 +1,54 @@
+<?php include('partials/menu.php'); ?>
+        <!-- Main content section starts-->
+        <div class="main-content">
+            <div class="wrapper">
+            <h1>Manage food</h1>
+            <!-- Button to Add Food -->
+
+            <br />  <br />  
+            <a href="#" class="btn-primary">Add food</a>
+            <br />  <br />  <br />
+
+
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N</th>
+                    <th>Full name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Alina Omorbekova</td>
+                    <td>alinaOmorbekova</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update admin</a>
+                        <a href="#" class="btn-danger">Delete admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Alina Omorbekova</td>
+                    <td>alinaOmorbekova</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update admin</a>
+                        <a href="#" class="btn-danger">Delete admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>Alina Omorbekova</td>
+                    <td>alinaOmorbekova</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update admin</a>
+                        <a href="#" class="btn-danger">Delete admin</a>
+                    </td>
+                </tr>
+            </table>
+            </div>
+        </div>
+        <!-- Main content section ends -->
+        
+
+    </body>
+    <?php include('partials/footer.php'); ?>
