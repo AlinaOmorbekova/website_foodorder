14a26f21f65c99cff9717a3d6c6b5fed09b7d7bc
Alina <46772913+AlinaOmorbekova@users.noreply.github.com>
2021-05-13 19:59:41 +0600

    Create constants.php

---
+++ config/constants.php
@@ -0,0 +1,19 @@
+<?php
+//Start Session
+
+session_start();
+
+//Create Constants to store non repeating values
+
+define('SITEURL', 'http://localhost/DolceVita/');
+
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'food-order');
+
+
+$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_connect_error()); //Database Connection
+$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_connect_error()); //Selecting database
+
+?>
