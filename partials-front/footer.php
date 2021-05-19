d2bd525e930198dd27b95118fe6203c6db7234d5
Alina <46772913+AlinaOmorbekova@users.noreply.github.com>
2021-05-20 01:44:21 +0600

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,28 @@
+   <!-- social Section Starts Here -->
+   <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/DolceVitaOsh/"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/dolcevitaosh/"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.tripadvisor.ru/Restaurant_Review-g800374-d13434184-Reviews-Dolce_Vita-Osh.html"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>2021. All rights reserved.</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
